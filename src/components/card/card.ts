4c556c620d2c415f3ad623a8cdc21e1bc4a1a5c1
Liangtj <liangtjsky@gmail.com>
2018-05-16 16:06:55 +0800

    card邏輯完成，等待一些樣式修改

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -11,17 +11,13 @@ export default {
   data: () => {
     return {
       postId: 'pa1b2c3d4',
-      title: '美國制裁中興',
       author: '王寶寶',
+      title: '美國制裁中興',
       content: '中兴案，其实合规细节没那么重要 - 目前看违规事实没有争议。所以，美国政府的处罚级别才重要。对于美国政府的各种要求，中兴这种背景的中国企业，有各种违规，其实是双方心知肚明的事情 - 中国的死敌当中，又有多少美国企业提供技术和设备呢？中兴违反了美国政府的游戏规则无疑，就像中国政府做出口合规，也一定能抓住违反规则的美国企业一样。处罚，为了政府信用也必须落实。但是，对此，到底是寻求一个双方可接受的妥协，还是要往死里掐？这次的威胁，是核弹级别的。.美国是全球化的受益者。而且，可能仍然是最大的受益者。借助一个负责任，开放和全球化的美国，让全球都可以放心的使用一些来自美国的底层技术架构。全球 IT 基础解决方案，很多都来自硅谷。这种架构让美国的硅谷占据了整个 IT 产业链的顶端。最大化了硅谷可能获得的市场。同时，提升了全球技术创新的效率。多赢',
-      likeCountPost: 15,
-      commentCount: 4,
-      // comments:[
-      //   {id: 'liangtjsky@163.com', comment: '這不好吧', likeNum: 3},
-      //   {id: 'binly42@gmail.com', comment: '媽的，快看一人之下啊，我丟'},
-      //   {id: 'jackylrd@163.com', comment: '對不起！！！'},
-      //   {id: 'lqh@outlook.com', comment: '對不起！！！'},
-      // ],
+      likeCountPost: 9,
+      commentCount: 2,
+
+      loading: true,
       reportReason: '',
       currentUserLikePost: false,
       comments: [],
@@ -29,6 +25,14 @@ export default {
       showMoreComments: false,
       inputComment: '',
       inputting: false
+    }
+  },
+  computed: {
+    previewComment() {
+      return this.comments.slice(0,3 > this.comments.length ? this.comments.length : 3 )
+    },
+    totalTitle() {
+      return this.author + ' > ' + this.title;  
     }
   },
   async beforeMount()  {
@@ -53,6 +57,7 @@ export default {
   methods: {
     async init() {
       await this.getComments();
+      this.loading = false;
     },
     async getComments() {
       try {
@@ -61,7 +66,7 @@ export default {
         this.comments = res.data.data.comments;
       } catch(err) {
         console.log(err)
-        this.openNotificationWithIcon('error','獲取評論失敗');
+        this.openNotificationWithIcon('error','獲取评论失敗');
       }
     },
     openNotificationWithIcon(type: string, title: string, descrip: string) {
@@ -80,7 +85,7 @@ export default {
       }
     },
     onIgnore() {
-
+      this.openNotificationWithIcon('info', '此功能日後再說');
     },
     async onReportToPost() {
       try {
@@ -95,9 +100,14 @@ export default {
         let res = await toggleLikePost(this.postId);
         this.currentUserLikePost = res.data.data.currentUserLike;
         this.likeCountPost = res.data.data.currentLikeCount;
-        this.openNotificationWithIcon('success', '點贊／取贊成功');
+        if(res.data.data.currentUserLike) {
+          this.openNotificationWithIcon('success', '點贊成功');
+        } else {
+          this.openNotificationWithIcon('success', '取消点赞成功');
+        }
+        // this.openNotificationWithIcon('success', '點贊／取贊成功');
       } catch(e) {
-        this.openNotificationWithIcon('error', '點贊／取贊失敗');
+        this.openNotificationWithIcon('error', '操作失敗');
       }
     },
     async onReportToComment(commentId: string, reason: string = 'no reason') {
@@ -111,17 +121,21 @@ export default {
     async toggleLikeComment(commentId: string) {
       try {
         let res = await toggleLikeComment(this.postId, commentId);
-        this.openNotificationWithIcon('success', '點贊／取贊成功');
+        if(res.data.data.currentUserLike) {
+          this.openNotificationWithIcon('success', '點贊成功');
+        } else {
+          this.openNotificationWithIcon('success', '取消点赞成功');
+        }
       } catch(e) {
-        this.openNotificationWithIcon('error', '點贊／取贊失敗');
+        this.openNotificationWithIcon('error', '操作失敗');
       }
     },
     async onComment() {
       try {
         let res = await commentToPost(this.postId, this.inputComment);
-        this.openNotificationWithIcon('success','評論成功');
+        this.openNotificationWithIcon('success','评论成功');
       } catch(e) {
-        this.openNotificationWithIcon('error','評論失敗');
+        this.openNotificationWithIcon('error','评论失敗');
       } finally {
         this.getComments();
       }
--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -1,65 +1,68 @@
 <template lang='pug'>
 .card-container
-  v-card(:title='title' :bordered='false' style='width: 400px')
-    .card-header(slot='extra')
-      .concrete
-      .setting
-        v-dropdown(trigger='click' placement='bottomRight')
-          a.ant-dropdown-link(href='javascript:;' slot='title')
-            | 更多
-            v-icon(type='down')
-          v-dropdown-menu(slot='menu')
-            v-dropdown-item(index='0')
-              a.follow(@click='onStar') 关注帖子
-            v-dropdown-item(index='1')
-              a.ignore(@click='onIgnore') 忽略帖子
-            v-dropdown-item(index='2')
-              a.report(@click='onReportToPost') 举报滥用行为
-    .card-content
-      .text
-        .total-text(v-if='content.length <= 52') {{ content }}
-        .trim-text(v-else)
-          .not-read-more(v-if='!readMore')
-            .trim-text-content {{ content.slice(0, 52)}}
-            a.more(@click='readMore = true') 阅读全文
-          .read-more(v-else)
-            .total-text {{ content }}
-            a.more-text( @click='readMore = false') 收起
+  .loading(v-if='loading')
+    v-card(style="width: 400px" :title="totalTitle" loading)
+  .loaded(v-else)
+    v-card(:title='totalTitle' :bordered='false' style='width: 400px')
+      .card-header(slot='extra')
+        .concrete
+        .setting
+          v-dropdown(trigger='click' placement='bottomRight')
+            a.ant-dropdown-link(href='javascript:;' slot='title')
+              | 更多
+              v-icon(type='down')
+            v-dropdown-menu(slot='menu')
+              v-dropdown-item(index='0')
+                a.follow(@click='onStar') 关注帖子
+              v-dropdown-item(index='1')
+                a.ignore(@click='onIgnore') 忽略帖子
+              v-dropdown-item(index='2')
+                a.report(@click='onReportToPost') 举报滥用行为
+      .card-content
+        .text
+          .total-text(v-if='content.length <= 52') {{ content }}
+          .trim-text(v-else)
+            .not-read-more(v-if='!readMore')
+              .trim-text-content {{ content.slice(0, 52)}}
+              a.more(@click='readMore = true') 阅读全文
+            .read-more(v-else)
+              .total-text {{ content }}
+              a.more-text( @click='readMore = false') 收起
 
-      //- .img(v-if='imageLink')
-        img(:src='imageLink')
-    .card-footer
-      .others-comments(v-if='comments.length !== 0')
-        a.more(@click='showMoreComments = !showMoreComments')
-          | {{ '显示/关闭更多评论&ensp;(共' + comments.length + '條)' }}
-        .preview(v-if='!showMoreComments')
-          .comment(v-for='comment in comments.slice(0,3 > comments.length ? comments.length : 3 )')
-            span.username {{ comment.author + ':&ensp;'}}
-            span {{ comment.content }}
-        .total-comments(v-else)
-          .comment(v-for='comment in comments')
-            .username-wrapper
-              span.username {{ comment.author + '&ensp;'}}
-              span(v-if='comment["like_count"] && comment["like_count"] !== 0')
-                | {{ '+' + comment["like_count"] }}
-            .comment-content {{ comment.content }}
-            .interactive
-              a.like(@click='toggleLikeComment(comment.commentId)') +1
-              a.report(@click='onReportToComment(comment.commentId)') 举报
-      .draft-editor
-        .before-input(v-if='!inputting')
-          .input
-            v-input(placeholder="发表评论" @input='inputting = true')
-          .like(@click='toggleLikePost')
-            v-button(type='default' icon='like')
-              span {{ likeCountPost }}
-          .share
-        .inputting(v-else)
-          .input
-            v-input(placeholder="发表评论" v-model='inputComment')
-          .to-do
-            a.option.cancel(@click='inputting = false') 取消
-            a.option.confirm(@click='onComment') 发布
+        //- .img(v-if='imageLink')
+          img(:src='imageLink')
+      .card-footer
+        .others-comments(v-if='comments.length !== 0')
+          a.more(@click='showMoreComments = !showMoreComments')
+            | {{ '显示/关闭更多评论&ensp;(共' + comments.length + '條)' }}
+          .preview(v-if='!showMoreComments')
+            .comment(v-for='comment in previewComment')
+              span.username {{ comment.author + ':&ensp;'}}
+              span {{ comment.content }}
+          .total-comments(v-else)
+            .comment(v-for='comment in comments')
+              .username-wrapper
+                span.username {{ comment.author + '&ensp;'}}
+                span(v-if='comment["like_count"] && comment["like_count"] !== 0')
+                  | {{ '+' + comment["like_count"] }}
+              .comment-content {{ comment.content }}
+              .interactive
+                a.like(@click='toggleLikeComment(comment.commentId)') +1
+                a.report(@click='onReportToComment(comment.commentId)') 举报
+        .draft-editor
+          .before-input(v-if='!inputting')
+            .input
+              v-input(placeholder="发表评论" @input='inputting = true')
+            .like(@click='toggleLikePost')
+              v-button(type='default' icon='like')
+                span {{ likeCountPost }}
+            .share
+          .inputting(v-else)
+            .input
+              v-input(placeholder="发表评论" v-model='inputComment')
+            .to-do
+              a.option.cancel(@click='inputting = false') 取消
+              a.option.confirm(@click='onComment') 发布
 </template>
 
 <script src='./card.ts' lang='ts'></script>
